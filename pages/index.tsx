c201ecb89d036b700b6c8375604bcd3c5e632839
Joe McCann <joemccann@Joes-MBP.home>
2021-12-14 15:31:16 -0500

    Navbar component created

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,30 @@
 import type { NextPage } from 'next';
 
+import styled, { ThemeConsumer } from 'styled-components';
+
 import {
 	Heading1,
 	SubHeading2,
 } from '../components/GlobalThemeComponents.styled';
 
+const Nav = styled.nav`
+	display: flex;
+	justify-conent: space-between;
+	align-items: center;
+	width: 100vw;
+
+	@media (min-width: 768px) {
+		height: 96px;
+	}
+
+	@media (min-width: 1224px) {
+	}
+`;
+
 const Home: NextPage = () => {
 	return (
 		<>
+			<Nav></Nav>
 			<main>
 				<Heading1>Earth</Heading1>
 				<SubHeading2>EUROPA</SubHeading2>
