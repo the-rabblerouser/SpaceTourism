80f8c1dd73d941ebe95fb08c552795965c5204a4
Joe McCann <joemccann@Joes-MBP.home>
2021-12-14 16:41:38 -0500

    Desktop navbar created

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,83 @@
 import type { NextPage } from 'next';
 
-import styled, { ThemeConsumer } from 'styled-components';
+import styled from 'styled-components';
 
-import {
-	Heading1,
-	SubHeading2,
-} from '../components/GlobalThemeComponents.styled';
+import { Logo } from '../lib/icons/Logo';
+
+const Body = styled.div`
+	height: 100vh;
+	background-color: #000;
+	background-image: url('/assets/home/background-home-desktop.jpg');
+	background-position: center;
+	background-repeat: no-repeat;
+	background-size: cover;
+`;
 
 const Nav = styled.nav`
+	@media (min-width: 768px) {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		width: 100vw;
+		height: 96px;
+	}
+
+	@media (min-width: 1224px) {
+	}
+`;
+
+const LogoContainer = styled.div`
 	display: flex;
-	justify-conent: space-between;
+	justify-content: center;
 	align-items: center;
-	width: 100vw;
+	height: 100%;
+	width: 100px;
 
 	@media (min-width: 768px) {
-		height: 96px;
+	}
+
+	@media (min-width: 1224px) {
+	}
+`;
+
+const NavLinkContainer = styled.div`
+	@media (min-width: 768px) {
+		display: flex;
+		align-items: center;
+		justify-content: space-evenly;
+		height: 100%;
+		background-color: rgba(225, 225, 225, 0.04);
+		width: 450px;
+	}
+
+	@media (min-width: 1224px) {
+		display: flex;
+		align-items: center;
+		justify-content: space-evenly;
+		height: 100%;
+		width: 830px;
+	}
+`;
+
+const NavLink = styled.a`
+	font-family: ${({ theme }) => theme.barlow};
+	color: ${({ theme }) => theme.white};
+	font-size: 16px;
+	letter-spacing: 2.7px;
+
+	@media (min-width: 768px) {
+	}
+
+	@media (min-width: 1224px) {
+	}
+`;
+
+const Span = styled.span`
+	font-family: ${({ theme }) => theme.barlow};
+	font-weight: bold;
+	color: ${({ theme }) => theme.white};
+
+	@media (min-width: 768px) {
 	}
 
 	@media (min-width: 1224px) {
@@ -24,11 +87,29 @@ const Nav = styled.nav`
 const Home: NextPage = () => {
 	return (
 		<>
-			<Nav></Nav>
-			<main>
-				<Heading1>Earth</Heading1>
-				<SubHeading2>EUROPA</SubHeading2>
-			</main>
+			<Body>
+				<Nav>
+					<LogoContainer>
+						<Logo />
+					</LogoContainer>
+
+					<NavLinkContainer>
+						<NavLink>
+							<Span>00</Span> HOME
+						</NavLink>
+						<NavLink>
+							<Span>01</Span> DESTINATION
+						</NavLink>
+						<NavLink>
+							<Span>02</Span> CREW
+						</NavLink>
+						<NavLink>
+							<Span>03</Span> TECHNOLOGY
+						</NavLink>
+					</NavLinkContainer>
+				</Nav>
+				<main></main>
+			</Body>
 		</>
 	);
 };
